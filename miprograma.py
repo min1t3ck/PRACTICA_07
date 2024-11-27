5fafabb74b8d71c4f5886a4cd90fa0cbf697def3
V1ct0r45 <victors.ramireza@gmail.com>
2024-11-26 20:35:18 -0600

    server manda mensaje, no recibe

--- miprograma.py
+++ miprograma.py
@@ -2,22 +2,62 @@ from Ui_msn_v1 import *
 from Ui_Primera import * 
 from Ui_Segunda import *
 from PyQt6.QtWidgets import QMainWindow, QDialog, QApplication
+from PyQt6.QtCore import QThread, pyqtSignal
 import sys
+import socket
+
+class ThreadSocket(QThread):
+    global connected
+    signal_message = pyqtSignal(str)
+    def __init__(self, host, port, name):
+        global connected
+        super().__init__()
+        server.connect(('18.119.116.177', 3003))
+        connected = True
+        server.send(bytes(f"<name>{name}", 'utf-8'))
+
+    def run(self):
+        global connected
+        try:
+            while connected:
+                message = server.recv(BUFFER_SIZE)
+                if message:
+                    self.signal_message.emit(message.decode("utf-8"))
+                else:
+                    self.signal_message.emit("<!!disconected!!>")
+                    break
+                
+        except ...:
+            self.signal_message.emit("<!!error!!>")
+        finally:
+            server.close()
+            connected = False
+        
+    def stop(self):
+        global connected
+        connected = False
+        self.wait()
+
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, *parent, **flags) -> None:
         super().__init__(*parent, **flags)
         self.setupUi(self)
-        self.Enviar_chat_general.clicked.connect(self.EnviarGrupal)
-
-    def EnviarGrupal(self):
-        "Agregar funcionalidades del servidor"
-        mensaje = self.texto_chat_general.text().strip()
-        if mensaje:  # Validar que el mensaje no esté vacío
-            self.MostrarGrupal(mensaje)
+        
+        self.Enviar_chat_general.clicked.connect(self.mensaje_saliente)
+        self.texto_chat_general.returnPressed.connect(self.mensaje_saliente)
+        
+    def mensaje_saliente(self):
+        str = self.texto_chat_general.text()
+        if str != "" and connected:
+            server.send(bytes(str, 'utf-8'))
             self.texto_chat_general.clear()
-        else:
-            self.MostrarAdvertencia("El mensaje no puede estar vacío.")
+            self.mensage_entrante("<Tú> " + str + '\n')
+            
+    def mensage_entrante(self, mensaje):
+        self.chat_general.setPlainText(self.chat_general.toPlainText() + mensaje)
+        self.chat_general.verticalScrollBar().setValue(self.chat_general.verticalScrollBar().maximum())
+            
     
     def MostrarGrupal(self, mensaje):
         """
@@ -36,6 +76,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         Mostrar advertencia al usuario en caso de errores.
         """
         QMessageBox.warning(self, "Advertencia", texto)
+        
+        
 
                
 class Primera(QDialog, Ui_DialogPrim):
@@ -48,7 +90,10 @@ class Primera(QDialog, Ui_DialogPrim):
     def abrir_principal(self):
         """Cerrar esta ventana para continuar con Principal."""
         "Agregar funciones para verificacion de usuario"
-        self.accept()  # Cierra `Primera` e informa al bucle principal
+        self.accept()  # Cierra `Primera` e informa al bucle principa
+        port = 3003
+        user = 'vic'
+        self.coneccion = ThreadSocket('18.119.116.177', int(port), user)
 
     def abrir_segunda(self):
         """Abrir la ventana secundaria como un diálogo modal."""
@@ -78,6 +123,9 @@ class Segunda(QDialog, Ui_DialogSeg):
 
 
 if __name__ == "__main__":
+    BUFFER_SIZE = 1024  # Usamos un número pequeño para tener una respuesta rápida
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connected = False
     app = QtWidgets.QApplication(sys.argv)
     while True:
         # Instanciar y mostrar `Primera`
