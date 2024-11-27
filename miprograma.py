72af1a99bb9a73d5fff883f337c5a77aa44f763d
Osvaldowo <osvaldo.prodigy@gmail.com>
2024-11-26 23:55:15 -0600

    Ya quedó por completo la conexión con el servidor, agregue los cambios que hizo Bic al programa principal, solo falta corregir los errores en la visualización de los mensajes

--- miprograma.py
+++ miprograma.py
@@ -1,14 +1,75 @@
 from Ui_msn_v1 import *
 from Ui_Primera import * 
 from Ui_Segunda import *
-from PyQt6.QtWidgets import QMainWindow, QDialog, QApplication
+from PyQt6.QtWidgets import QMainWindow, QDialog, QApplication, QMessageBox
+from PyQt6.QtCore import QThread, pyqtSignal
 import sys
+import socket
+
+class ThreadSocket(QThread):
+    global connected
+    signal_message = pyqtSignal(str)
+    def __init__(self, host, port, name):
+        global connected
+        super().__init__()
+        server.connect(('18.119.116.177', 3003))
+        connected = True
+        server.send(bytes(f"<name>{name}", 'utf-8'))
+
+    def run(self):
+        global connected
+        try:
+            while connected:
+                message = server.recv(BUFFER_SIZE)
+                if message:
+                    self.signal_message.emit(message.decode("utf-8"))
+                else:
+                    self.signal_message.emit("<!!disconected!!>")
+                    break
+                
+        except ...:
+            self.signal_message.emit("<!!error!!>")
+        finally:
+            server.close()
+            connected = False
+        
+    def stop(self):
+        global connected
+        connected = False
+        self.wait()
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, *parent, **flags) -> None:
         super().__init__(*parent, **flags)
         self.setupUi(self)
+        
+        self.msgSend.clicked.connect(self.mensaje_saliente)
+        self.msgWrite.returnPressed.connect(self.mensaje_saliente)
 
+    def mensaje_saliente(self):
+        str = self.msgWrite.text()
+        if str != "" and connected:
+            server.send(bytes(str, 'utf-8'))
+            self.msgWrite.clear()
+            self.mensaje_entrante("<Tú> " + str + '\n')
+            
+    def mensaje_entrante(self, mensaje):
+        self.msgView.setPlainText(self.msgView.toPlainText() + mensaje)
+        self.msgView.verticalScrollBar().setValue(self.msgView.verticalScrollBar().maximum())
+            
+    def MostrarGrupal(self, mensaje):
+        """Agregar mensaje al chat general"""
+        self.msgView.setPlainText(
+            self.msgView.toPlainText() + mensaje + "\n"
+        )
+
+        self.msgView.verticalScrollBar().setValue(
+            self.msgView.verticalScrollBar().maximum()
+        )
+    
+    def MostrarAdvertencia(self, texto):
+        """Mostrar advertencia al usuario en caso de errores"""
+        QMessageBox.warning(self, "Advertencia", texto)
 
 class Primera(QDialog, Ui_DialogPrim):
     def __init__(self, *args, **kwargs):
@@ -20,6 +81,9 @@ class Primera(QDialog, Ui_DialogPrim):
     def abrir_principal(self):
         """Cerrar esta ventana para continuar con Principal."""
         self.accept()  # Cierra `Primera` e informa al bucle principal
+        port = 3003
+        user = 'vic'
+        self.coneccion = ThreadSocket('18.119.116.177', int(port), user)
 
     def abrir_segunda(self):
         """Abrir la ventana secundaria como un diálogo modal."""
@@ -37,13 +101,21 @@ class Segunda(QDialog, Ui_DialogSeg):
         super(Segunda, self).__init__(*args, **kwargs)
         self.setupUi(self)
         self.btn3.clicked.connect(self.cerrar)
+        '''self.btnCrear_Usuario.clicked.connect(self.CrearUsuario)'''
 
     def cerrar(self):
         """Cerrar la ventana secundaria."""
         self.close()
 
-
+    def CrearUsuario(self):
+        usuario_nuevo = self.Nuevo_usuario.text()
+        contraseña_usuario = self.Contra2.text()
+        
+    
 if __name__ == "__main__":
+    BUFFER_SIZE = 1024  # Usamos un número pequeño para tener una respuesta rápida
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connected = False
     app = QtWidgets.QApplication(sys.argv)
     while True:
         # Instanciar y mostrar `Primera`
