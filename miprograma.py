079c684dc4ee73392d051c8c7003eb302308a11a
Julio_Arreguin <davidhedez139@gmail.com>
2024-11-29 17:05:23 -0600

    inicio de la implementacion de ambos chats

--- miprograma.py
+++ miprograma.py
@@ -11,10 +11,12 @@ from PyQt6.QtCore import QThread, pyqtSignal
 import sys
 import socket
 
+
+
 class ThreadSocket(QThread):
-    global connected
-    signal_message = pyqtSignal(str)
-    def __init__(self, host, port, name):
+    global connected                                #Variable global conected 
+    signal_message = pyqtSignal(str)                #Señal 
+    def __init__(self, host, port, name):           
         global connected
         super().__init__()
         server.connect(('18.119.116.177', 3003))
@@ -41,63 +43,13 @@ class ThreadSocket(QThread):
     def stop(self):
         global connected
         connected = False
-        self.wait()
+        self.wait() 
+ 
+ 
 
-class MainWindow(QMainWindow, Ui_MainWindow):
-    def __init__(self, socket_thread,*parent, **flags) -> None:
-        super().__init__(*parent, **flags)
-        self.setupUi(self)
-        self.coneccion = socket_thread
-        self.coneccion.signal_message.connect(self.mensaje_entrante)
-        
-        self.usuarios_conectados = []
-        
-        self.msgSend.clicked.connect(self.mensaje_saliente)
-        self.msgWrite.returnPressed.connect(self.mensaje_saliente)
-        
-        #botones auxiliares pra probar las ventanas de privado y grupal
-        self.btn_Private.clicked.connect(self.mensajePrivado)
-        self.btn_Group.clicked.connect(self.mensajeGrupo)
-        
-        self.btn_Tereneitor.clicked.connect(self.terreneitor)
-        
-        
 
-    def mensaje_saliente(self):
-        str = self.msgWrite.text()
-        if str != "" and connected:
-            server.send(bytes(str, 'utf-8'))
-            self.msgWrite.clear()
-            self.mensaje_entrante("<Tú> " + str + '\n')
-            
-    def mensaje_entrante(self, mensaje):
-        if mensaje.startswith("<list_response>"):
-        
-            usuarios = mensaje.removeprefix("<list_response>").removesuffix("</list_response>")
-            self.usuarios_conectados = usuarios.split(",") if usuarios else []
-            print("Usuarios conectados:", self.usuarios_conectados)
-        else:
-            
-            self.msgView.setPlainText(self.msgView.toPlainText() + mensaje)
-            self.msgView.verticalScrollBar().setValue(self.msgView.verticalScrollBar().maximum())
-            
-    
-    def MostrarAdvertencia(self, texto):
-        """Mostrar advertencia al usuario en caso de errores"""
-        QMessageBox.warning(self, "Advertencia", texto)
 
-    #abre privado
-    def mensajePrivado(self):
-        self.ventana_privada = Privado(self) 
-        self.ventana_privada.show() 
-    #abre grupal
-    def mensajeGrupo(self):
-        self.ventana_privada = Grupo(self) 
-        self.ventana_privada.show() 
-        
-    def terreneitor(self):
-        self.ventana_terreneitor = Terrene(self)
-        self.ventana_terreneitor.show()
+#/////////////////////Primera Ventana (Inicio sesion)////////////////
 
 class Primera(QDialog, Ui_DialogPrim):
     def __init__(self, *args, **kwargs):
@@ -133,8 +85,11 @@ class Primera(QDialog, Ui_DialogPrim):
         """Cerrar todo el programa si se cierra esta ventana."""
         QApplication.instance().quit()  # Garantiza salida completa
         event.accept()
+#////////////////////////////////////////////////////////////////////////
 
 
+
+#//////////////////////SEGUNDA VENTANA///////////////////////////////////
 class Segunda(QDialog, Ui_DialogSeg):
     def __init__(self, parent=None, *args, **kwargs):
         super(Segunda, self).__init__(*args, **kwargs)
@@ -149,7 +104,75 @@ class Segunda(QDialog, Ui_DialogSeg):
     def CrearUsuario(self):
         usuario_nuevo = self.Nuevo_usuario.text()
         contraseña_usuario = self.Contra2.text()
+#////////////////////////////////////////////////////////////////////////
+
         
+#////////////////////////VENTANA (PRI)ncipal//////////////////////////////
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, socket_thread,*parent, **flags) -> None:
+        super().__init__(*parent, **flags)
+        self.setupUi(self)
+        self.coneccion = socket_thread
+        self.coneccion.signal_message.connect(self.mensaje_entrante)
+        
+        self.usuarios_conectados = []
+        
+        self.msgSend.clicked.connect(self.mensaje_saliente)
+        self.msgWrite.returnPressed.connect(self.mensaje_saliente)
+        
+        #botones auxiliares pra probar las ventanas de privado y grupal
+        self.btn_Private.clicked.connect(self.mensajePrivado)
+        self.btn_Group.clicked.connect(self.mensajeGrupo)
+
+        self.btn_Tereneitor.clicked.connect(self.terreneitor)
+        
+        
+
+    def mensaje_saliente(self):
+        str = self.msgWrite.text()
+        if str != "" and connected:
+            server.send(bytes(str, 'utf-8'))
+            self.msgWrite.clear()
+            self.mensaje_entrante("<Tú> " + str + '\n')
+            
+            
+            
+    def mensaje_entrante(self, mensaje):
+        if mensaje.startswith("<list_response>"):
+        
+            usuarios = mensaje.removeprefix("<list_response>").removesuffix("</list_response>")
+            self.usuarios_conectados = usuarios.split(",") if usuarios else []
+            print("Usuarios conectados:", self.usuarios_conectados)
+        else:
+            
+            self.msgView.setPlainText(self.msgView.toPlainText() + mensaje)
+            self.msgView.verticalScrollBar().setValue(self.msgView.verticalScrollBar().maximum())
+            
+    
+    
+    def MostrarAdvertencia(self, texto):
+        """Mostrar advertencia al usuario en caso de errores"""
+        QMessageBox.warning(self, "Advertencia", texto)
+
+
+    #abre privado
+    def mensajePrivado(self):
+        self.ventana_privada = Privado(self) 
+        self.ventana_privada.show() 
+    
+    
+    #abre grupal
+    def mensajeGrupo(self):
+        self.ventana_privada = Grupo(self) 
+        self.ventana_privada.show() 
+      
+        
+    def terreneitor(self):
+        self.ventana_terreneitor = Terrene(self)
+        self.ventana_terreneitor.show()
+
+
+
 #Ventana Mensaje privado        
 class Privado(QDialog, Ui_Privado):
     signal_enviar = pyqtSignal(str, str)
@@ -161,6 +184,25 @@ class Privado(QDialog, Ui_Privado):
     def cerrarVentana(self):
         self.close()
 
+
+
+#Ventana mensaje grupal
+class Grupo(QDialog, Ui_Grupo):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)        
+        self.btn_Close.clicked.connect(self.cerrarVentana)
+
+    def cerrarVentana(self):
+        self.close()
+
+
+
+
+#***********************************************************************
+#*********************FUNCIONALIDADES ADICIONALES***********************
+#***********************************************************************
+#//////////////////////////TERRENEITOR/////////////////////////////////
 class Terrene(QDialog, Ui_Terreneitor):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -196,20 +238,11 @@ class Terrene(QDialog, Ui_Terreneitor):
         
     def parar(self):
         server.send(bytes('<command>Parar', 'utf-8'))
-    
-    
+#/////////////////////////////////////////////////////////////////////////    
+  
+  
+  
 
-
-#Ventana mensaje grupal
-class Grupo(QDialog, Ui_Grupo):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setupUi(self)        
-        self.btn_Close.clicked.connect(self.cerrarVentana)
-
-    def cerrarVentana(self):
-        self.close()
-    
 if __name__ == "__main__":
     BUFFER_SIZE = 1024  # Usamos un número pequeño para tener una respuesta rápida
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -217,10 +250,8 @@ if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     while True:
         # Instanciar y mostrar `Primera`
-        print("hola")
         ventana_primera = Primera()
         resultado = ventana_primera.exec()  # Bloquea hasta cerrar `Primera`
-        print("adios")
 
         if resultado == QDialog.DialogCode.Accepted:
             # Abrir `Principal` si el usuario presionó "Siguiente"
